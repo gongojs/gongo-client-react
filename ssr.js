df9f01a3b20fe194b3db4306fd50429f609b9d1b
Gadi Cohen <dragon@wastelands.net>
2021-11-28 10:50:40 +0000

    add ssr stub

---
+++ ssr.js
@@ -0,0 +1,47 @@
+const React = require('react');
+
+//const { useGongoCursor, useGongoLive, useGongoOne, useGongoUserId } = require('./liveQueries');
+//const { useGongoSub, useGongoIsPopulated, IsPopulated } = require('./subscriptions');
+const db = require('gongo-client');
+
+function useGongoCursor() {
+  
+}
+
+function useGongoLive() {
+  
+}
+
+function useGongoOne() {
+  
+}
+
+function useGongoUserId() {
+  
+}
+
+function useGongoSub() {
+  
+}
+
+function useGongoIsPopulated() {
+  
+}
+
+function IsPopulated() {
+  
+}
+
+module.exports = {
+  __esModule: true,
+  db,
+
+  useGongoCursor,
+  useGongoLive,
+  useGongoOne,
+  useGongoUserId,
+
+  useGongoSub,
+  useGongoIsPopulated,
+  IsPopulated,
+};
